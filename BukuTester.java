e3e597519999283f6e8f65704e4dbb9d184405ed
nurmalaaisn <nurmala_amir_33rpl@student.smktelkom-mlg.sch.id>
2024-08-22 17:17:52 +0700

    Add files via upload

---
+++ BukuTester.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class BukuTester {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       
+        System.out.println("Inputkan judul buku : ");
+        String judul = in.nextLine();
+        System.out.println("Inputkan pengarang buku : ");
+        String pengarang = in.nextLine();
+        System.out.println("Inputkan ID buku : ");
+        int id = in.nextInt();
+        System.out.println("Inputkan tahun terbit : ");
+        int tahunTerbit = in.nextInt();
+
+
+        Buku perpus = new Buku(id,judul,pengarang,tahunTerbit);
+        System.out.println(perpus.judul);
+        System.out.println(perpus.pengarang);
+        System.out.println(perpus.id);
+        System.out.println(perpus.tahunTerbit);
+
+         //object dengan constructor parameter
+         /*Buku Mala = new Buku(345, "Alster Lake", "Aurie", 2022);
+         Buku Alfi = new Buku(412, "Bumi", "Tere Liye", 2014);
+         Buku Jeli = new Buku(686, "Malioboro at Midnight", "Skysphire", 2023);
+         Buku Kyla = new Buku(167, "Butterflies", "Ale", 2021);
+         Buku Ligia = new Buku(832, "Hello", "Tere Liye", 2023);*/
+
+        /*System.out.println("Mala Meminjam Buku : ");
+        System.out.println("Id = "+Mala.id);
+        System.out.println("Judul = "+Mala.judul);
+        System.out.println("Pengarang = "+Mala.pengarang);
+        System.out.println("Tahun Terbit = "+Mala.tahunTerbit);
+        
+        System.out.println("Alfi Meminjam Buku : ");
+        System.out.println("Id = "+Alfi.id);
+        System.out.println("Judul = "+Alfi.judul);
+        System.out.println("Pengarang = "+Alfi.pengarang);
+        System.out.println("Tahun Terbit = "+Alfi.tahunTerbit);
+        
+        System.out.println("Jeli Meminjam Buku : ");
+        System.out.println("Id = "+Jeli.id);
+        System.out.println("Judul = "+Jeli.judul);
+        System.out.println("Pengarang = "+Jeli.pengarang);
+        System.out.println("Tahun Terbit = "+Jeli.tahunTerbit);
+       
+        System.out.println("Kyla Meminjam Buku : ");
+        System.out.println("Id = "+Kyla.id);
+        System.out.println("Judul = "+Kyla.judul);
+        System.out.println("Pengarang = "+Kyla.pengarang);
+        System.out.println("Tahun Terbit = "+Kyla.tahunTerbit);
+        
+        System.out.println("Ligia Meminjam Buku : ");
+        System.out.println("Id = "+Ligia.id);
+        System.out.println("Judul = "+Ligia.judul);
+        System.out.println("Pengarang = "+Ligia.pengarang);
+        System.out.println("Tahun Terbit = "+Ligia.tahunTerbit);*/
+
+        in.close();
+
+    }
+}
